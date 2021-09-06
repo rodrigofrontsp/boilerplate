e4ce725dd5c12f94e1afbc075935b004963bebb3
rodrigofrontsp <rodrigo.goncalves@outlook.com>
2021-09-06 16:10:47 -0300

    alteracao name projeto para client

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,8 +16,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    color: #333;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;    
   }
 `
 
