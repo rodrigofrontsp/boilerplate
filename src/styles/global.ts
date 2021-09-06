39221e556c371de9adbf4ff5cd797d4824322fd2
rodrigofrontsp <rodrigo.goncalves@outlook.com>
2021-09-06 16:05:55 -0300

    alteracao styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,7 +16,8 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color: #333;
   }
 `
 
